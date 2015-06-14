5b22f4328f6e603ada360357ffc331156f88b318
dannyvassallo <danielvassallo87@gmail.com>
2015-06-14 16:56:46 -0400

    defined working overlay process

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,13 +1,9 @@
-# encoding: utf-8
-
 class FileUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
   storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -16,36 +12,21 @@ class FileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  
+  process :overlay
 
+  def overlay
+    manipulate! format: "jpg" do |source|
+      overlay_path = Rails.root.join("app/assets/images/homer_overlay.gif")
+      overlay = Magick::Image.read(overlay_path).first
+      source = source.resize_to_fill(320, 320)
+      source.composite!(overlay, 0, 0, Magick::OverCompositeOp)
+    end
+  end
 end
